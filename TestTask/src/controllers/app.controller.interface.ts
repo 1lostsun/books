57c69c1869e72e28f68b86416b9696c211da1b61
1lostsun <149478863+1lostsun@users.noreply.github.com>
2024-07-20 17:24:36 +0300

    оптимизация

--- TestTask/src/controllers/app.controller.interface.ts
+++ TestTask/src/controllers/app.controller.interface.ts
@@ -1,10 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 
 export interface IAppController {
-	validateBookID: (req: Request, res: Response, next: NextFunction) => Promise<boolean>;
-	validateBooks: (req: Request, res: Response, next: NextFunction) => Promise<boolean>;
-	bookInfo: (req: Request, res: Response, next: NextFunction) => Promise<Response>;
-	createBook: (req: Request, res: Response, next: NextFunction) => Promise<Response>;
+	validateBookID: (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+		func: Function,
+	) => Promise<Response>;
+	validateBooks: (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+		func: Function,
+	) => Promise<Response>;
+	bookInfo: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	createBook: (req: Request, res: Response, next: NextFunction) => Promise<void | Response>;
 	updateBook: (req: Request, res: Response, next: NextFunction) => Promise<void | Response>;
-	deleteBook: (req: Request, res: Response, next: NextFunction) => Promise<Response>;
+	deleteBook: (req: Request, res: Response, next: NextFunction) => Promise<void | Response>;
 }
--- TestTask/src/controllers/app.controller.ts
+++ TestTask/src/controllers/app.controller.ts
@@ -13,36 +13,18 @@ class AppController implements IAppController {
 		this.deleteBook = this.deleteBook.bind(this);
 	}
 
-	async validateBooks(req: Request, res: Response, next: NextFunction): Promise<boolean> {
+	async validateBooks(
+		req: Request,
+		res: Response,
+		next: NextFunction,
+		func: Function,
+	): Promise<Response> {
 		const { title, genre, author, publicationDate } = req.body;
 		const [result] = await db.query(
 			`SELECT EXISTS (SELECT FROM books where title = $1 AND genre = $2 AND publicationDate = $3 AND author = $4)`,
 			[title, genre, publicationDate, author],
 		);
-		return result.exists;
-	}
-
-	async validateBookID(req: Request, res: Response, next: NextFunction): Promise<boolean> {
-		const { id } = req.params;
-		const booksId = id.split(':');
-		const [result] = await db.query(`SELECT EXISTS (SELECT FROM books where id = $1)`, [
-			booksId[1],
-		]);
-		return result.exists;
-	}
-	async bookInfo(req: Request, res: Response, next: NextFunction): Promise<Response> {
-		const result = this.validateBookID(req, res, next);
-		if (!result) {
-			return res.status(404).send(`book doesn't exist`);
-		} else {
-			const book: object = await model.getInfo(req, res, next);
-			return res.json(book);
-		}
-	}
-
-	async createBook(req: Request, res: Response, next: NextFunction): Promise<Response> {
-		const result = await this.validateBooks(req, res, next);
-		if (result) {
+		if (result.exists) {
 			return res.status(400).send(`the book already exists`);
 		} else {
 			const book = await model.create(req, res, next);
@@ -50,24 +32,44 @@ class AppController implements IAppController {
 		}
 	}
 
-	async updateBook(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
-		const result = await this.validateBookID(req, res, next);
-		if (!result) {
+	async validateBookID(
+		req: Request,
+		res: Response,
+		next: NextFunction,
+		func: Function,
+	): Promise<Response> {
+		const { id } = req.params;
+		const booksId = id.split(':');
+		const [result] = await db.query(`SELECT EXISTS (SELECT FROM books where id = $1)`, [
+			booksId[1],
+		]);
+		if (!result.exists) {
 			return res.status(404).send(`book doesn't exist`);
 		} else {
-			const book = await model.updateInfo(req, res, next);
-			this.bookInfo(req, res, next);
+			const book = await func(req, res, next);
+			if (func === model.delete) {
+				return book;
+			} else if (func === model.updateInfo) {
+				return res.send(`book was updated`);
+			} else {
+				return res.json(book);
+			}
 		}
 	}
+	async bookInfo(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+		await this.validateBookID(req, res, next, model.getInfo);
+	}
 
-	async deleteBook(req: Request, res: Response, next: NextFunction): Promise<Response> {
-		const result = await this.validateBookID(req, res, next);
-		if (!result) {
-			return res.status(404).send(`book doesn't exist`);
-		} else {
-			const book = await model.delete(req, res, next);
-			return res.json(book);
-		}
+	async createBook(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
+		await this.validateBooks(req, res, next, model.create);
+	}
+
+	async updateBook(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
+		this.validateBookID(req, res, next, model.updateInfo);
+	}
+
+	async deleteBook(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
+		await this.validateBookID(req, res, next, model.delete);
 	}
 }
 
